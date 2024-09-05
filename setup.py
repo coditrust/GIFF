4bf555b50c62582d9a73fd9247fdc03b7e71dc86
CodiTrust <89127943+coditrust@users.noreply.github.com>
2024-09-05 15:27:06 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='giff',
+    version='0.1.0',
+    author='Anthony Dessiatnikoff',
+    description='This project allows to get information from vulnerable systems',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    py_modules=['giff'],
+    url='https://github.com/coditrust/GIFF',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'colorama',
+    ],
+    entry_points={
+        'console_scripts': [
+            'giff=giff:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
+
